1a3392343d603dc882414df7a22c3e173a18fd7e
Alex.lee <meable.lee@gmail.com>
2022-09-23 17:19:07 +0800

    feat: create develop branch

--- app/src/main/java/com/oriente/aptsample/Print2.java
+++
@@ -1,7 +0,0 @@
-package com.oriente.aptsample;
-
-import com.oriente.anno.PrintInfo;
-
-@PrintInfo(value = "print2", active = BuildConfig.active)
-public class Print2 {
-}
--- app/src/main/java/com/oriente/aptsample/Router1.java
+++
@@ -1,7 +0,0 @@
-package com.oriente.aptsample;
-
-import com.oriente.anno.Route;
-
-@Route(name = "router1")
-public class Router1 {
-}

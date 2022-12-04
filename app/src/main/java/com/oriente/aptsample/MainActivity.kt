223b03d1cf887e27400ec1f8e5fe90dccfa0069a
Alex.lee <meable.lee@gmail.com>
2022-12-04 12:21:48 +0800

    code

--- app/src/main/java/com/oriente/aptsample/MainActivity.kt
+++ app/src/main/java/com/oriente/aptsample/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.oriente.aptsample
 
 import android.os.Bundle
+import androidx.annotation.IntDef
 import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        test(ALEX)
 
     }
+
+    companion object {
+        const val ALEX = 0
+        const val LEI = 1
+    }
+
+    @IntDef(value = [ALEX, LEI])
+    @Retention(AnnotationRetention.SOURCE)
+    @Target(AnnotationTarget.VALUE_PARAMETER)
+    annotation class Teacher()
+
+    fun test(@Teacher teacher: Int) {}
 }

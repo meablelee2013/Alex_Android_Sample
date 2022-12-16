0d20f48873168f548185dfaba3cc7a308a2a6db6
meablelee2013 <meable.lee@gmail.com>
2022-12-16 21:00:40 +0800

    feat: code

--- app/src/main/java/com/oriente/aptsample/MainActivity.kt
+++ app/src/main/java/com/oriente/aptsample/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.oriente.aptsample
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toMaxMB = SizeUnit.BYTE.toMB(Runtime.getRuntime().maxMemory())
+        val toTotalMB = SizeUnit.BYTE.toMB(Runtime.getRuntime().totalMemory())
+        Log.d("alex", "max memory $toMaxMB  total memory $toTotalMB")
 
     }
 }
